eed2d65675fcca5dcfa47b3e3d2de507845b356e
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2025-03-14 01:06:43 +0100

    update: inject-wallet

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import  { WagmiProvider} from 'wagmi'
+import { config } from './wagmi.js' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <WagmiProvider config={config}>
     <App />
+    </WagmiProvider>
   </StrictMode>,
 )
---
+++ src/wagmi.js
@@ -0,0 +1,19 @@
+import { http, createConfig } from 'wagmi'
+import { base, mainnet, sepolia } from 'wagmi/chains'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+
+const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [
+    injected(),
+    walletConnect({ projectId }),
+    metaMask(),
+    safe(),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+})
